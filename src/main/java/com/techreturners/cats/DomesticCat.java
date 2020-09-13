526176822d5924db7648bd24f6a6645b555c813a
sanae <sanae.k1986@gmail.com>
2020-09-13 22:38:15 +0100

    refactor code

--- src/main/java/com/techreturners/cats/DomesticCat.java
+++ src/main/java/com/techreturners/cats/DomesticCat.java
@@ -1,21 +1,22 @@
 package com.techreturners.cats;
+
 public class DomesticCat extends WildCat{
 
 
 public DomesticCat(){
   //call wildcat constructor 
-        super(false,"Purrrrrrr",false,23,"domestic");
-    }
+    super(false,"Purrrrrrr",false,23,"domestic");
+}
 
 //in case the cas is full the sound should be diffirent
 public String isFull(){
-   super.SetEat("miow");
-   return super.eat();    
+    super.SetEat("miow");
+    return super.eat();    
 }
 
 //in case the cat is bit bigger or smaller than avarage 
 public void setHeight(int height){
-super.Setheight(height);
+    super.Setheight(height);
 }
 
 
